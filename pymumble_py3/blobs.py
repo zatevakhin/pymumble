d9186b4f389af9690c0e8d12ae5317d581994b80
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2025-04-10 00:44:33 +0100

    fix: import in blobs.py

--- pymumble_py3/blobs.py
+++ pymumble_py3/blobs.py
@@ -2,7 +2,7 @@
 import struct
 
 from .constants import *
-from .mumble_pb2 import RequestBlob
+from .Mumble_pb2 import RequestBlob
 
 
 class Blobs(dict):
@@ -11,16 +11,16 @@ class Blobs(dict):
     """
     def __init__(self, mumble_object):
         self.mumble_object = mumble_object
-        
+
     def get_user_comment(self, hash):
         """Request the comment of a user"""
         if hash in self:
             return
         request = RequestBlob()
         request.session_comment.extend(struct.unpack("!5I", hash))
-        
+
         self.mumble_object.send_message(PYMUMBLE_MSG_TYPES_REQUESTBLOB, request)
-    
+
     def get_user_texture(self, hash):
         """Request the image of a user"""
         if hash in self:
@@ -28,9 +28,9 @@ class Blobs(dict):
 
         request = RequestBlob()
         request.session_texture.extend(struct.unpack("!5I", hash))
-        
+
         self.mumble_object.send_message(PYMUMBLE_MSG_TYPES_REQUESTBLOB, request)
-    
+
     def get_channel_description(self, hash):
         """Request the description/comment of a channel"""
         if hash in self:
@@ -38,5 +38,5 @@ class Blobs(dict):
 
         request = RequestBlob()
         request.channel_description.extend(struct.unpack("!5I", hash))
-        
+
         self.mumble_object.send_message(PYMUMBLE_MSG_TYPES_REQUESTBLOB, request)
